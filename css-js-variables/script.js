2fb4b305d04469d65c5b2c682a807ad3d4211573
paulyo19 <catkillercleary@gmail.com>
2019-09-18 21:23:59 +0100

    finished project

--- css-js-variables/script.js
+++ css-js-variables/script.js
@@ -2,7 +2,7 @@ const inputs = document.querySelectorAll('.controls input');
 
 function handleUpdate() {
     const suffix = this.dataset.sizing || '';
-    document.documentElement.style.setProperty(`--${this.name}`, this.value)
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
 }
 
 inputs.forEach(input => input.addEventListener('change', handleUpdate));
