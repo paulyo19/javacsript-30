2fdadd7197389a974e8d66963d769a770449ab67
paulyo19 <catkillercleary@gmail.com>
2019-09-30 18:47:26 +0100

    using the some and every method in array to find out if the people are over or at a certain age

--- array-2/script.js
+++ array-2/script.js
@@ -12,10 +12,28 @@ const people = [
     { text: 'Nice Nice Nice!', id: 542328 }
   ];
 
-  
+
   // Some and Every Checks
   // Array.prototype.some() // is at least one person 19 or older?
-  // Array.prototype.every() // is everyone 19 or older?
+  const isAdult = people.some(function(person){
+      const currentYear = (new Date()).getFullYear();
+      if(currentYear - person.year >= 19){
+          return true;
+        }
+    })
+    
+    console.log(isAdult)
+    // short hand
+    // const isAdult = people.some(person => {
+    //     const currentYear = (new Date()).getFullYear();
+    //     return currentYear - person.year >= 19;
+    // })
+    
+    
+    // Array.prototype.every() // is everyone 19 or older?
+    const allAdults = people.every(person => ((new Date()).getFullYear()) - person.year >= 19);
+        console.log(allAdults)
+    
   // Array.prototype.find()
   // Find is like filter, but instead returns just the one you are looking for
   // find the comment with the ID of 823423
