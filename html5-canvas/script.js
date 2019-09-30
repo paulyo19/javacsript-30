9761c02bf469cc0764560763f2d500c47e5c0333
paulyo19 <catkillercleary@gmail.com>
2019-09-30 19:55:59 +0100

    adding mouse events and offsets

--- html5-canvas/script.js
+++ html5-canvas/script.js
@@ -1,23 +1,38 @@
-const canvas = document.querySelector('#draw')
+const canvas = document.querySelector('#draw');
 
-const ctx = canvas.getContext('2d')
+const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-ctx.strokeStyle = '#BADA55';               // color
+ctx.strokeStyle = "#FF0000";             // color
 ctx.lineJoin = 'round';                   //when a line meets another line
 ctx.lineCap = 'round';
+ctx.lineWidth = 100;
 
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
+let hue = 0;
 
 function draw(e) {
     if(!isDrawing) return;    //stop the function from running when they are not moused down
-    console.log(e)
+    console.log(e);
+    ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+    ctx.beginPath();
+    ctx.moveTo(lastX, lastY);
+    ctx.lineTo(e.offestX, e.offsetY);
+    ctx.stroke();
+    lastX = e.offestX;
+    lastY = e.offsetY;
 }
 
+canvas.addEventListener('mousedown', (e) => {
+    isDrawing = true;
+    [lastX, lastY] = [e.offestX, e.offsetY]
+});
+
 canvas.addEventListener('mousemove', draw)
-canvas.addEventListener('mousedown', () => isDrawing = true )
-canvas.addEventListener('mouseup', () => isDrawing = false )
-canvas.addEventListener('mouseout', () => isDrawing = false )
+canvas.addEventListener('mouseup', () => isDrawing = false );
+canvas.addEventListener('mouseout', () => isDrawing = false );
+
+
