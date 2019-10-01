f650ec4d098428230569f63b7fd439bee8ad3765
paulyo19 <catkillercleary@gmail.com>
2019-10-01 19:46:26 +0100

    finished project

--- checkbox/script.js
+++ checkbox/script.js
@@ -5,6 +5,23 @@ let lastChecked;
 
 function handleCheck(e) {
     // console.log(e)
+    // CHECK IF THEY HAD THE SHIFT KEY DOWN AND CHECK IF THEY ARE CHECKING IT
+    let inBetween = false;
+    if (e.shiftKey && this.checked) {
+        // GO AHEAD AND DO WHAT WE PLEASE
+        // LOOP OVER EVERY SINGLE CHECKBOX
+        checkboxes.forEach(checkbox => {
+            console.log(checkbox)
+            if (checkbox === this || checkbox === lastChecked) {
+                inBetween = !inBetween;
+                console.log('checking inbetween')
+            }
+            if (inBetween) {
+                checkbox.checked = true
+            }
+        })
+
+    }
     lastChecked = this;
 }
 
