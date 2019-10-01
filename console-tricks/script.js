a0787c113f2e961b9f12e3316f79069926dfe7e2
paulyo19 <catkillercleary@gmail.com>
2019-10-01 18:53:39 +0100

    finished small project

--- console-tricks/script.js
+++ console-tricks/script.js
@@ -43,9 +43,27 @@ console.log(p)
 console.dir(p)
 
 // Grouping together
-
+dogs.forEach(dog => {
+    console.group(`${dog.name}`)
+    console.log(`this is ${dog.name}`);
+    console.log(`this is ${dog.name} is ${dog.age} years old`);
+    console.log(`this is ${dog.name} is ${dog.age * 7} dog years old`);
+    console.groupEnd(`${dog.name}`)
+});
 
 // counting
-
+console.count('wes')
+console.count('wes')
+console.count('wes')
+console.count('wes')
+console.count('wes')
 
 // timing
+console.time('fetching data')
+fetch('https://api.github.com/users/wesbos')
+    .then(data => data.json())
+    .then(data => {
+        console.timeEnd(data)
+    })
+
+    console.table(dogs)
