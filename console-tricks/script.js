4f4cd148eee611922fe561777616ddff3449ebb0
paulyo19 <catkillercleary@gmail.com>
2019-10-01 18:42:54 +0100

    learning a few console tricks to help improve debugging

--- console-tricks/script.js
+++ console-tricks/script.js
@@ -9,14 +9,43 @@ function makeGreen() {
 
 
 // Regular
+// console.log('hello')
+
 // Interpolated
+// console.log('hello i am a %s string!', '@')
+// console.log(`hello i am ${var}`)
+
 // Styled
+// console.log('%c I am some great text', 'font-size: 50px')
+
 // warning!
+// console.warn('oh noo!')
+
 // Error :|
+// console.error('shit')
+
+
 // Info
+// console.info('crocodiles eat 3-4 people a year')
+
 // Testing
+const p = document.querySelector('p')
+console.assert(p.classList.contains('ouch'), 'that is wrong!')
+
+// console.assert(1 ===1, 'thats is wrong')
+// console.assert(1 ===2, 'thats is wrong')
+
 // clearing
+// console.clear();
+
 // Viewing DOM Elements
+console.log(p)
+console.dir(p)
+
 // Grouping together
+
+
 // counting
+
+
 // timing
