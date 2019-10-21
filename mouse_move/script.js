c3c62d4d5b0439f97f46eb78313fc76eb3c82ee2
paulyo19 <catkillercleary@gmail.com>
2019-10-21 18:53:05 +0100

    adding a bit of math to this project

--- mouse_move/script.js
+++ mouse_move/script.js
@@ -1,14 +1,19 @@
 const hero = document.querySelector('.hero')
 const text = hero.querySelector('h1')
+const walk = 100;               //100px
 
 function shadow(e){
     const { offsetWidth: width, offsetHeight: height} = hero;
     let { offsetX: x, offsetY: y } = e;
     
     if(this !== e.target){
-        
+        x = x + e.target.offsetLeft
+        y = y + e.target.offsetTop
     }
+    const xWalk = (x / width * walk) - (walk / 2)
+    const yWalk = (y / height * walk) - (walk / 2)   //this means that the walk is 100 so 50 is as high is what we should go and -50 is how low we should go
 
+    console.log()
 }
 
 hero.addEventListener('mousemove', shadow)
