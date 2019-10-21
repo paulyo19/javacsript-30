9c06189ca1a6704250a97b71cc7ed270d56697e1
paulyo19 <catkillercleary@gmail.com>
2019-10-21 18:47:03 +0100

    changing mistake on line 2 in js file

--- mouse_move/script.js
+++ mouse_move/script.js
@@ -1,7 +1,13 @@
 const hero = document.querySelector('.hero')
-const hero = hero.querySelector('h1')
+const text = hero.querySelector('h1')
 
 function shadow(e){
+    const { offsetWidth: width, offsetHeight: height} = hero;
+    let { offsetX: x, offsetY: y } = e;
+    
+    if(this !== e.target){
+        
+    }
 
 }
 
