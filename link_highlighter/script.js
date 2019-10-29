f9e3b67bbb47c5ad493e13b752d9edadfb733ce1
paulyo19 <catkillercleary@gmail.com>
2019-10-29 18:04:14 +0000

    adding some js

---
+++ link_highlighter/script.js
@@ -0,0 +1,13 @@
+const triggers = document.querySelectorAll('a');
+const highlight = document.createElement('span');
+highlight.classList.add('highlight');
+document.body.appendChild(highlight);
+function highlightLink() {
+  const linkCoords = this.getBoundingClientRect();
+  console.log(linkCoords);
+  const coords = {
+    width: linkCoords.width,
+    height: linkCoords.height,
+    top: linkCoords.top + window.scrollY,
+    left: linkCoords.left + window.scrollX
+  };
