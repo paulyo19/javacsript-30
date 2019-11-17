10b1c084fcfe6a00b39596f4bf85064d243304e7
paulyo19 <catkillercleary@gmail.com>
2019-11-17 19:47:30 +0000

    finishing up the project

--- stirpe_follow_alon_nav/script.js
+++ stirpe_follow_alon_nav/script.js
@@ -15,3 +15,13 @@ function handleEnter() {
       top: dropdownCoords.top - navCoords.top,
       left: dropdownCoords.left - navCoords.left
     };
+    background.style.setProperty('width', `${coords.width}px`);
+    background.style.setProperty('height', `${coords.height}px`);
+    background.style.setProperty('transform', `translate(${coords.left}px, ${coords.top}px)`);
+  }
+  function handleLeave() {
+    this.classList.remove('trigger-enter', 'trigger-enter-active');
+    background.classList.remove('open');
+  }
+  triggers.forEach(trigger => trigger.addEventListener('mouseenter', handleEnter));
+  triggers.forEach(trigger => trigger.addEventListener('mouseleave', handleLeave));
