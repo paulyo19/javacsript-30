63f388e1da0d24e461571dc37346711812aaa2fa
paulyo19 <catkillercleary@gmail.com>
2019-11-18 21:43:46 +0000

    adding an eventlistner

--- click_drag/script.js
+++ click_drag/script.js
@@ -2,3 +2,10 @@ const slider = document.querySelector('.items');
   let isDown = false;
   let startX;
   let scrollLeft;
+
+  slider.addEventListener('mousedown', (e) => {
+    isDown = true;
+    slider.classList.add('active');
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+  });
