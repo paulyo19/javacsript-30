388acee0dacb825c94f626bbbc20efcad473f2cf
paulyo19 <catkillercleary@gmail.com>
2019-11-26 18:08:12 +0000

    creating two functions with time left and end time

--- countdown_timer/script.js
+++ countdown_timer/script.js
@@ -23,3 +23,19 @@ function timer(seconds) {
       displayTimeLeft(secondsLeft);
     }, 1000);
   }
+
+  function displayTimeLeft(seconds) {
+    const minutes = Math.floor(seconds / 60);
+    const remainderSeconds = seconds % 60;
+    const display = `${minutes}:${remainderSeconds < 10 ? '0' : '' }${remainderSeconds}`;
+    document.title = display;
+    timerDisplay.textContent = display;
+  }
+  
+  function displayEndTime(timestamp) {
+    const end = new Date(timestamp);
+    const hour = end.getHours();
+    const adjustedHour = hour > 12 ? hour - 12 : hour;
+    const minutes = end.getMinutes();
+    endTime.textContent = `Be Back At ${adjustedHour}:${minutes < 10 ? '0' : ''}${minutes}`;
+  }
