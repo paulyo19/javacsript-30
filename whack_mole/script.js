f43e6852ea7a3c69767561d83e340ff0b063a497
paulyo19 <catkillercleary@gmail.com>
2019-11-27 07:37:41 +0000

    creating functions

--- whack_mole/script.js
+++ whack_mole/script.js
@@ -4,3 +4,17 @@ const holes = document.querySelectorAll('.hole');
   let lastHole;
   let timeUp = false;
   let score = 0;
+
+  function randomTime(min, max) {
+    return Math.round(Math.random() * (max - min) + min);
+  }
+  function randomHole(holes) {
+    const idx = Math.floor(Math.random() * holes.length);
+    const hole = holes[idx];
+    if (hole === lastHole) {
+      console.log('Ah nah thats the same one bud');
+      return randomHole(holes);
+    }
+    lastHole = hole;
+    return hole;
+  }
